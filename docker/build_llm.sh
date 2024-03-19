a7624e823e118b48a9aa4a44e5abb3d8d6669f50
Weiguang Han <hanweiguang1@gmail.com>
2024-03-19 14:15:58 +0800

    update docker

--- docker/build_llm.sh
+++ docker/build_llm.sh
@@ -2,6 +2,6 @@ export https_proxy=http://127.0.0.1:65530
 export http_proxy=http://127.0.0.1:65530
 export all_proxy=socks5://127.0.0.1:65530
 
-docker build --network host --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg all_proxy=$all_proxy -t llm -f llm_cu118.dockerfile .
+docker build --network host --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg all_proxy=$all_proxy -t llm_cu118 -f llm_cu118.dockerfile .
 
-docker build --network host --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg all_proxy=$all_proxy -t llm -f llm_cu121.dockerfile .
+docker build --network host --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg all_proxy=$all_proxy -t llm_cu121 -f llm_cu121.dockerfile .
